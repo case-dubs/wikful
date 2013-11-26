6915b172fa84f203ebeb1fbd83b55c5ef237e467
case-dubs <casey594@gmail.com>
2013-11-26 09:59:47 -0800

    first wikful commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikful::Application.config.secret_key_base = '7f9b6579df31be5cfa359e8df41c497cc7f8a03a31a7bb21b860bc2e75ba8a95899775a271f679bfaf99a58e62ff9cb306f331082898cb84b3f0f0c1253efbcf'
